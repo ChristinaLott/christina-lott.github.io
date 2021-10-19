0167c1aaa2dafebfb3dd251fadcffefa6d066267
ChristinaLott <christina.s.lott@gmail.com>
2021-10-19 20:23:00 +0000

    Completed Matchy Step 2

--- projects/matchy/data.js
+++ projects/matchy/data.js
@@ -29,28 +29,37 @@ console.log(animal);
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 /**1. [X] Create a variable named `noises` and assign it to an empty array.
- 2. [X] Using **bracket notation** give `noises` it's first element. A string representing a sound your animal might make.
+ 2. [] Using **bracket notation** give `noises` it's first element. A string representing a sound your animal might make.
  3. [X] Using an array function add another noise to the end of `noises`.
  4. [ ] Go to the array documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array?redirectlocale=en-US
  5. [X] Look through the functions until you find the one that will place an element at the begining of the array.
  6. [X] Add an element to `noises` using this function.
- 7. [X] Using **bracket syntax** again, add another element to the end of `noises`. Make sure that the way you do this step would work no matter how many elements `noises` had. In other words, don't hard code the position of the new element.
- 8. [ ] `console.log` the length of `noises`
- 9. [ ] `console.log` the last element in `noises` again without hard coding the index.
- 10. [ ] `console.log` the whole array.
+ 7. [ X] Using **bracket syntax** again, add another element to the end of `noises`. Make sure that the way you do this step would work no matter how many elements `noises` had. In other words, don't hard code the position of the new element.
+ 8. [X] `console.log` the length of `noises`
+ 9. [ X] `console.log` the last element in `noises` again without hard coding the index.
+ 10. [X ] `console.log` the whole array.
  11. [ ] Does it look right?
  */
 
 var noises = [];
 noises[0] = "woof";
-noises.push("growl");
+noises.push("growl"); 
 noises.unshift("bark");
-noises[noises.length - 1]="howl";
+noises[noises.length]="howl"; 
+console.log (noises[noises.length]);
+console.log(noises);
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+
+/**1. [ ] Using **bracket syntax**, assign the `noises` property on `animal` to our new `noises` array.
+2. [ ] Using any syntax add another noise to the `noises` property on `animal`.
+3. [ ] `console.log` `animal`.
+4. [ ] Does it look right?
+*/
+
 
 
 
