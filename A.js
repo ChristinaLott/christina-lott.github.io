0c3a73f264995c0b93bd10bdd4d83e561a1c04ec
ChristinaLott <christina.s.lott@gmail.com>
2021-11-02 01:08:50 +0000

    Installed Project Recursion

--- A.js
+++ A.js
@@ -23,3 +23,37 @@ _.pluck = function(arrObj, property){
     }
     return _.map(arrObj, func); //<--map invokes func here (even though it's a given argument...???), so calling back
  }
+
+
+
+ /**
+  * Below is an example of using recursion
+  */
+ 
+var nums = [1, 2, 3, 4];
+
+function nonrecursiveSum(arr){
+  var sum = 0;
+  for (var i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+  return sum;
+}
+
+function recursiveSum(arr){
+  //base case - condition for when function should stop
+  if(arr.length === 0){
+    return 0; //returning 0 because don't want to throw off sum so use 0 to not effect sum
+    //OR could return arr[0] AND have the length === 1, because [0] has no value
+  }
+
+  //recursion
+  return arr[0] + recursiveSum(arr.slice(1)); //this is a return chain
+}
+
+
+//return 1 + recursiveSum([2, 3, 4])
+//return 1 + 2 + recursiveSum ([3, 4])
+//return 1 + 2 + 3 + recursiveSum([4])
+//return
+    //each time call array use slice to nab stuff
