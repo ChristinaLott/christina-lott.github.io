1d0507c0abda405e63b2fae627770065d879591b
ChristinaLott <christina.s.lott@gmail.com>
2021-11-03 22:12:32 +0000

    WIP chessboard of eloquent javascript chap. 2

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -47,8 +47,32 @@ function fizzBuzz() {
 // drawChessboard //////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function drawChessboard() {
+/* 
+Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. 
+At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
 
+Passing this string to console.log should show something like this:
+
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # #
+
+When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.*/
+
+//use newline to seperate each line
+//use either a space " " or "#" in grid
+//should first work with binding size of 8 then later any number/size
+//output: console.log 8x8 grid of "#" and " "
+
+function drawChessboard() {
+  for(var i = " #"; i.length < 8; i += " #"){
+    console.log (i + \n); 
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
