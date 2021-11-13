a9365eee8dbe1124717b3a3b2c219bc5c3f0366c
ChristinaLott <christina.s.lott@gmail.com>
2021-11-13 02:03:14 +0000

    WIP Studies 8:03pm

--- projects/Studies/Variables.js
+++ projects/Studies/Variables.js
@@ -1,4 +1,59 @@
 /**
+ *Intro for each section
+ *Clear explanation for each concept
+ *Working code examples for each concept
+ *Comment on what each code example should log/return
+ *https://docs.google.com/presentation/d/1CXbrZgjJDJvalWdccZAkWQREPY6EeunTBhjIbIqG3Bg/embed?slide=id.g5a8f2e46ea_0_69
+ * 
+ * Variables:
+ *  -Declaration and Assignment
+ *  -var, let, const
+ *  -Hoisting
+ * Data Types:
+ *  -Number
+ *  -String
+ *  -Boolean
+ *  -Array
+ *  -Object
+ *  -Function
+ *  -undefined
+ *  -null
+ *  -NaN
+ *  -Inifinity and -Infinity (google 'em)  
+ *  -What is the difference between primitive simple and complex data types?
+ *  -Primitive values are passed to a function by COPY, complex values by REFERENCE. What does it mean, and how are they different?
+ * Operators
+ *  -Assignment operators
+ *  -Arthemitic operators
+ *  -Comparison operators
+ *  -Logical operators
+ *  -Unary operators (!, typeOf, -)
+ *  -Ternary operator (a ? b : c)
+ * String Manipulation
+ *  -With operators
+ *  -With string methods
+ * Control Flow
+ *  -If
+ *  -Else-if
+ *  -Else
+ *  -Switch
+ * Loops
+ *  -Explain while, for, for-in loops
+ *  -Be able to loop any number of times, forward counting up to some limit, backward counting down to 0
+ *  -Loop over an Array, forwards, backwards
+ *  -Loop over an Object
+ * Functions
+ *  -The two phases to use functions: First we must ___? Next we can execute (or two other words for executing a function?) a function by?
+ *  -What's the difference between a function's parameters and arguments PASSED to a function?
+ *  -What's the syntax for a NAMED function?
+ *  -How do we assign a function to a variable?
+ *  -Functions can OPTIONALLY take inputs, and OPTIONALLY return a single value. How do we specify inputs, and how do we specify outputs?
+ *  -Scope: Functions can see and modify variables in parent or global scopes. The inverse is NOT true.
+ *  -Closures: Functions form closures around the data they house. If an object returned from the Function and is held in memory somewhere (referenced), that closure stays ALIVE, and data can continue to exist in these closures!
+ */
+
+
+/**
  * 
  * reference the slides on what to put in. Due at the end of bootcamp
  * BUT don't let it fall by the wayside! Need to have notes and code examples!
